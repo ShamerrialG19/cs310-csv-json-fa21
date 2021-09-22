2d09ab5cc6e52444d945a5a22c817c340d5c2978
Shamerrial Garrett <sgarrett3@stu.jsu.edu>
2021-09-22 12:16:02 -0500

    Completed Project 1 of converting jsontoCSV and vice versa using the converter tools in netbeans.

--- src/edu/jsu/mcis/Converter.java
+++ src/edu/jsu/mcis/Converter.java
@@ -8,9 +8,9 @@ import org.json.simple.parser.*;
 
 public class Converter {
     
-    /*
     
-        Consider the following CSV data:
+    
+        /*Consider the following CSV data:
         
         "ID","Total","Assignment 1","Assignment 2","Exam 1"
         "111278","611","146","128","337"
@@ -22,7 +22,7 @@ public class Converter {
         "111276","579","130","111","338"
         "111241","973","236","237","500"
         
-        The corresponding JSON data would be similar to the following (tabs and
+        /*The corresponding JSON data would be similar to the following (tabs and
         other whitespace have been added for clarity).  Note the curly braces,
         square brackets, and double-quotes!  These indicate which values should
         be encoded as strings, and which values should be encoded as integers!
@@ -67,17 +67,53 @@ public class Converter {
             List<String[]> full = reader.readAll();
             Iterator<String[]> iterator = full.iterator();
             
-            // INSERT YOUR CODE HERE
             
-        }        
+            
+            // new empty containers
+            JSONObject json = new JSONObject();         
+            JSONArray rowHeaders = new JSONArray();
+            JSONArray colHeaders = new JSONArray();
+            JSONArray data = new JSONArray();
+            JSONArray holder;
+            String[] record = iterator.next();
+            
+            // loop to go through columns
+           for(int i = 0; i < record.length; i++){
+                colHeaders.add(record[i]);
+            }
+            
+            // while loop go through all data until false or condition is no 
+            //longer true
+            
+            while(iterator.hasNext()){
+                holder = new JSONArray();
+                record = iterator.next();
+                rowHeaders.add(record[0]);
+                for(int i = 1; i < record.length; i++){
+                    int stringHolder = Integer.parseInt(record[i]);
+                    holder.add(stringHolder);
+            }
+                // adds all revised data to holder container
+                    data.add(holder);
+                 
+                    
+            }
+
+         
+            json.put("colHeaders", colHeaders); // add column headers to JSON object
+            json.put("rowHeaders", rowHeaders); // add row headers to JSON object
+            json.put("data", data); // add "big array" of data to JSON object
+            results = JSONValue.toJSONString(json); // "flatten" JSON object to a JSON string
+     
+        }
         catch(Exception e) { e.printStackTrace(); }
         
-        return results.trim();
+        return results.trim(); // return JSON string
         
     }
     
     public static String jsonToCsv(String jsonString) {
-        
+         
         String results = "";
         
         try {
@@ -85,8 +121,48 @@ public class Converter {
             StringWriter writer = new StringWriter();
             CSVWriter csvWriter = new CSVWriter(writer, ',', '"', '\\', "\n");
             
-            // INSERT YOUR CODE HERE
             
+            //utilization of variables for JSonArray to put data in
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(jsonString);
+            
+            JSONArray colHeaders = (JSONArray) json.get("colHeaders");
+            JSONArray rowHeaders = (JSONArray) json.get("rowHeaders");
+            
+            JSONArray data = (JSONArray) json.get("data");
+            JSONArray holder;
+            
+            String [] record = new String[colHeaders.size()];
+            
+            // for loop for the amount of columns 
+            for(int i = 0; i < colHeaders.size(); i++){
+                record[i] = (String) colHeaders.get(i);
+            
+           }                   
+           csvWriter.writeNext(record);
+           
+           // for loop puts data in rows
+           for(int i = 0; i < data.size(); i++){
+               
+               // stores grades into overall data container and inserts it into JSONARRAY
+                holder = (JSONArray) data.get(i);
+                //adds rows 
+                record = new String[holder.size() + 1];
+                // initializes first column to  hold id's , assignments, etc
+                record[0] =(String) rowHeaders.get(i);
+                
+                // for loop for grades in each row excepts the inital row wit id's
+                for(int j = 0; j < holder.size(); j++){
+                    record[j + 1] = Long.toString((long)holder.get(j));
+                          
+           }
+                 // adds following rows to records
+                 csvWriter.writeNext(record);
+                          
+            
+        } 
+           
+           results = writer.toString();
         }
         
         catch(Exception e) { e.printStackTrace(); }
@@ -94,5 +170,6 @@ public class Converter {
         return results.trim();
         
     }
+   
 
 }
